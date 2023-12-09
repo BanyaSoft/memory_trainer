3fa5ccc7577d87d5e35060adfafa2524847b44f2
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2023-12-09 21:55:30 +0300

    Extra Protection fix
    This commit adds extra protection for the script users. Using the script multiple will not make dictionary files incorrect, they will stay correct

--- dictionaries/convert_file.py
+++ dictionaries/convert_file.py
@@ -1,18 +1,27 @@
 files = ['words5.txt', 'words6.txt', 'words7.txt', 'words8.txt']
 for file in files:
     try:
-        with open(file, 'r', encoding = 'utf-8') as inp:
-            resultString = ''
+        with open(file, 'r', encoding='utf-8') as inp:
+            resultString = ''  
             k = 0
+
+            try:
+                first_line = inp.readline()
+                count = int(first_line)
+
+            except ValueError:
+                resultString += first_line.upper()
+                k += 1
             for line in inp.readlines():
                 resultString += line.upper()
-                k += 1       
-
+                k += 1
+        
         resultString = str(k) + '\n' + resultString
         
-        with open(file, 'w', encoding = 'utf-8') as out:
+        with open(file, 'w', encoding='utf-8') as out:
             out.write(resultString)
-        print('Данные успешно перезаписаны в файл!')
+            
+        print('Everything is good!')
             
     except FileNotFoundError:
-        print('Нет такого файла в директории! Попробуйте еще раз')
+        print('There is no such file in the directory. Try again.')
