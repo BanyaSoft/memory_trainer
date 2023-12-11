194d85f02215f803523c71c2072d4b542bc974c3
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2023-12-11 12:15:18 +0300

    Fixing convert_file.py
    Be careful with ANSI encoding

--- dictionaries/convert_file.py
+++ dictionaries/convert_file.py
@@ -1,25 +1,24 @@
 files = ['words5.txt', 'words6.txt', 'words7.txt', 'words8.txt']
 for file in files:
     try:
-        with open(file, 'r', encoding='utf-8') as inp:
+        with open(file, 'r', encoding='ANSI') as inp:
             resultString = ''  
             k = 0
+            first_line = inp.readline()
 
             try:
-                global first_line
-		first_line = inp.readline()
                 count = int(first_line)
-
             except ValueError:
                 resultString += first_line.upper()
                 k += 1
+
             for line in inp.readlines():
                 resultString += line.upper()
                 k += 1
         
         resultString = str(k) + '\n' + resultString
         
-        with open(file, 'w', encoding='utf-8') as out:
+        with open(file, 'w', encoding='ANSI') as out:
             out.write(resultString)
             
         print('Everything is good!')
